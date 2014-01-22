6c0d2fcc60d050dcadcedd6ac61f0ea341e1c861
Gil Forsyth <gilforsyth@gmail.com>
2014-01-21 22:57:08 -0500

    script to generated README from notebook files placed in lessons directory'

---
+++ gen-readme.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python2
+
+from glob import glob
+from urllib import quote
+import re
+
+format_item = '* [{name}]({url})'.format
+bb_url = 'https://github.com/mesnardo/AeroPython/blob/master/{}'.format
+
+def notebooks():
+    return glob('lessons/*.ipynb')
+
+def lesson_id(filename):
+    return int(re.search('[0-9]+', filename).group())
+
+def lesson_name(filename):
+    return filename.split('/')[1].split('.')[0].split('_')[2]
+
+def nb_url(filename):
+    raw_url = bb_url(quote(quote(filename)))
+    return 'http://nbviewer.ipython.org/urls/{}'.format(raw_url)
+
+def write_readme(nblist, fo):
+    fo.write('\n')
+    for nb in nblist:
+        name = lesson_name(nb)
+        url = nb_url(nb)
+        fo.write('{}\n'.format(format_item(name=name, url=url)))
+
+def main():
+    nblist = sorted(notebooks(), key=lesson_id)
+    with open('README.md', 'w') as fo:
+        write_readme(nblist, fo)
+
+if __name__ == '__main__':
+    main()
