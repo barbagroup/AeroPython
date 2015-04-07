77f0c00b515ab7003d4c595d4e40aa47b9305743
weymouth <gabriel.weymouth@gmail.com>
2015-04-07 21:49:37 +0100

    Updated function names to be more pythonic

--- lessons/VortexPanel.py
+++ lessons/VortexPanel.py
@@ -3,9 +3,9 @@ import numpy
 from matplotlib import pyplot
 
 # velocity component functions
-def getU( x, y, S, gamma ):
+def get_u( x, y, S, gamma ):
     return gamma/(2*numpy.pi)*(numpy.arctan((x-S)/y)-numpy.arctan((x+S)/y))
-def getV( x, y, S, gamma ):
+def get_v( x, y, S, gamma ):
     return gamma/(4*numpy.pi)*(numpy.log(((x+S)**2+y**2)/((x-S)**2+y**2)))
 
 # vortex panel class
@@ -24,44 +24,31 @@ class Panel:
     # get the velocity!
     def velocity( self, x, y, gamma=None ):
         if gamma is None: gamma = self.gamma # default gamma
-        xp,yp = self.transformXY( x, y )     # transform
-        up = getU( xp, yp, self.S, gamma )   # get up
-        vp = getV( xp, yp, self.S, gamma )   # get vp
-        return self.rotateUV( up, vp )       # rotate back
+        xp,yp = self.transform_xy( x, y )    # transform
+        up = get_u( xp, yp, self.S, gamma )  # get u prime
+        vp = get_v( xp, yp, self.S, gamma )  # get v prime
+        return self.rotate_uv( up, vp )      # rotate back
     
     # plot the panel
     def plot(self):
         return pyplot.plot(self.x,self.y,'k-',lw=2)
     
     # transform from global to panel coordinates
-    def transformXY( self, x, y ):
-        xp = x-self.xc
-        yp = y-self.yc
-        xpp = xp*self.sx+yp*self.sy
-        ypp = yp*self.sx-xp*self.sy
-        return [ xpp, ypp ]
+    def transform_xy( self, x, y ):
+        xt = x-self.xc               # shift x
+        yt = y-self.yc               # shift y
+        xp = xt*self.sx+yt*self.sy   # rotate x
+        yp = yt*self.sx-xt*self.sy   # rotate y
+        return [ xp, yp ]
     
     # rotate velocity back to global coordinates
-    def rotateUV( self, u, v):
-        up = u*self.sx-v*self.sy
-        vp = v*self.sx+u*self.sy
-        return [ up, vp ]
-
-# compute the flow field
-def flowVelocity(panels,x,y,alpha=0):
-    # get the uniform velocity ( make it the same size & shape as x )
-    u = numpy.cos(alpha)*numpy.ones_like(x)
-    v = numpy.sin(alpha)*numpy.ones_like(x)
-    
-    # add the velocity contribution from each panel
-    for p in panels:
-        u0,v0 = p.velocity(x,y)
-        u = u+u0
-        v = v+v0
-    return [u,v]
+    def rotate_uv( self, up, vp):
+        u = up*self.sx-vp*self.sy    # reverse rotate u prime
+        v = vp*self.sx+up*self.sy    # reverse rotate v prime
+        return [ u, v ]
 
 # plot the flow on a grid
-def plotFlow(panels,alpha=0,xmax=2,N_grid=100):
+def plot_flow(panels,alpha=0,xmax=2,N_grid=100):
     # define the grid
     X = numpy.linspace(-xmax, xmax, N_grid) # computes a 1D-array for x
     Y = numpy.linspace(-xmax, xmax, N_grid) # computes a 1D-array for y
@@ -96,7 +83,7 @@ def influence(panel_i,panel_j):
     return u*panel_i.sx+v*panel_i.sy
 
 # construct the linear system
-def constructAb(panels,alpha=0):
+def construct_A_b(panels,alpha=0):
     # construct matrix
     N_panels = len(panels)
     A = numpy.empty((N_panels, N_panels), dtype=float) # empty matrix
@@ -111,8 +98,57 @@ def constructAb(panels,alpha=0):
     return [A,b]
 
 # determine the vortex strength on a set of panels
-def solveGamma(panels,alpha=0):
-    A,b = constructAb(panels,alpha)    # construct linear system
+def solve_gamma(panels,alpha=0):
+    A,b = construct_A_b(panels,alpha)  # construct linear system
     gamma = numpy.linalg.solve(A, b)   # solve for gamma!
     for i,p_i in enumerate(panels):
         p_i.gamma = gamma[i]           # update panels
+
+# determine the vortex panel strength with Kutta Condition
+def solve_gamma_kutta(panels,alpha=0):
+    A,b = construct_A_b(panels,alpha)   # construct linear system
+    A[:,0] += 1                         # gamma[0]+ ...
+    A[:,len(panels)-1] += 1             # gamma[N-1]=0
+    gamma = numpy.linalg.solve(A, b)    # solve for gamma!
+    for i,p_i in enumerate(panels):
+        p_i.gamma = gamma[i]            # update panels
+
+
+### Geometries
+
+# make a circle
+def make_circle(N,t_c=1):
+    # define the end-points of the panels
+    x_ends = numpy.cos(numpy.linspace(0, -2*numpy.pi, N+1))
+    y_ends = numpy.sin(numpy.linspace(0, -2*numpy.pi, N+1))
+    y_ends *= t_c
+    
+    # define the panels
+    circle = numpy.empty(N, dtype=object)
+    for i in xrange(N):
+        circle[i] = Panel(x_ends[i], y_ends[i], x_ends[i+1], y_ends[i+1])
+    
+    return circle
+
+# make a jukowski foil
+def make_jukowski( N, dx = 0.2, dy = 0, dr = 0 ):
+    # define the circle
+    x_ends = numpy.cos(numpy.linspace(0, -2*numpy.pi, N+1))
+    y_ends = numpy.sin(numpy.linspace(0, -2*numpy.pi, N+1))
+    
+    # shift circle
+    r = numpy.sqrt((1+dx)**2+dy**2)+dr
+    x2_ends = r*x_ends-dx
+    y2_ends = r*y_ends-dy
+    r2_ends = x2_ends**2+y2_ends**2
+    
+    # apply jukowski mapping
+    x3_ends = x2_ends*(1+1./r2_ends)/2
+    y3_ends = y2_ends*(1-1./r2_ends)/2
+    
+    # define the panels
+    foil = numpy.empty(N, dtype=object)
+    for i in xrange(N):
+        foil[i] = Panel(x3_ends[i], y3_ends[i], x3_ends[i+1], y3_ends[i+1])
+    
+    return foil
